d3bcb0e1ce0568341909116ed3dc36cf174efd58
Tony Brown <tony@tonybrown.me>
2017-03-18 08:49:00 -0400

    set up webpack dev server and linting code on the fly

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -3,19 +3,19 @@ import { render } from 'react-dom'
 import '../public/normalize.css'
 import '../public/style.css'
 
-
 const App = React.createClass({
-	render () {
-		return (
-		  <div className='app'>
-			  <div className='landing'>
-				  <h1>svideo</h1>
-				  <input type='text' placeholder='Search' />
-					<a>or Browse all</a>
-				</div>
-			</div>
-		)
-	}
+  render () {
+    return (
+      <div className='app'>
+        <div className='landing'>
+          <h1>svideo</h1>
+          <input type='text' placeholder='Search' />
+          <a>or Browse all</a>
+        </div>
+      </div>
+    )
+  }
 })
 
 render(<App />, document.getElementById('app'))
+
