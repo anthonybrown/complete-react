6d40c9b02163ea844fddfc0aadcdc9ce7ff3e733
Tony Brown <tony@tonybrown.me>
2017-03-20 09:00:04 -0400

    make tile display images and description

--- js/Search.js
+++ js/Search.js
@@ -9,7 +9,14 @@ const Search = React.createClass({
         {/* <pre><code>{JSON.stringify(preload, null, 2)}</code></pre> */}
         {preload.shows.map((show) => {
           return (
-            <h3>{show.title}</h3>
+            <div className='show-card'>
+              <img src={`/public/img/posters/${show.poster}`} />
+              <div>
+                <h3>{show.title}</h3>
+                <h4>({show.year})</h4>
+                <p>{show.description}</p>
+              </div>
+            </div>
           )
         })}
       </div>
