aca4be91f5e32fa273058afc927fae32a433385b
Tony Brown <tony@tonybrown.me>
2017-03-20 08:54:40 -0400

    setup search and use map to iterate over the array

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { HashRouter, Match } from 'react-router'
+import { BrowserRouter, Match } from 'react-router'
 import Landing from './Landing'
 import Search from './Search'
 import '../public/normalize.css'
 import '../public/style.css'
 
-// using createClass instead of classes because it's easier?
-// I've written this component using Class and Const so all three work for this component
-
 const App = React.createClass({
   render () {
     return (
-      <HashRouter>
+      <BrowserRouter>
         <div className='app'>
           <Match exactly pattern='/' component={Landing} />
           <Match pattern='/search' component={Search} />
         </div>
-      </HashRouter>
+      </BrowserRouter>
     )
   }
 })
--- js/Search.js
+++ js/Search.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import preload from '../public/data.json'
 
 const Search = React.createClass({
   render () {
     return (
-      <h1>Search Page!</h1>
+      <div className='search'>
+        {/* 3 arguments are the data you want, a function, indentation */}
+        {/* <pre><code>{JSON.stringify(preload, null, 2)}</code></pre> */}
+        {preload.shows.map((show) => {
+          return (
+            <h3>{show.title}</h3>
+          )
+        })}
+      </div>
     )
   }
 })
